031ab6b92e470443cbb5506e7214564a193fad57
2urbanski <franek.urbanski@gmail.com>
2024-12-04 17:11:15 +0100

    ADC works - draws a line on the screen according to level on P0.24

--- LcdControl.h
+++ LcdControl.h
@@ -22,6 +22,7 @@ void init_lcd() {
 	lcdWriteReg(HADRPOS_RAM_END, LCD_MAX_X);
 	lcdWriteReg(VADRPOS_RAM_START, 0);
 	lcdWriteReg(VADRPOS_RAM_END, LCD_MAX_Y);
+	fill(LCDBlack);
 }
 
 void draw_point(uint16_t x, uint16_t y, uint16_t color) {
--- main.c
+++ main.c
@@ -6,20 +6,37 @@
 
 uint16_t buff[SCOPE_MAX_X] = {};
 
+void init_adc() {
+	LPC_SC->PCONP |= (1 << 12); // Enable ADC
+	LPC_ADC->ADCR = (1 << 1) 	| // ADC0.1
+									(1 << 21) | // ADC Operational
+									(5 << 8);   // Divide clock to 5 Mhz
+	LPC_PINCON->PINSEL1 |= (1 << 16); // P0.24 as ADC0.1
+}
+	
 void setup() {
 	init_lcd();
 	LED_Initialize();
 	LED_SetOut(0);
-	fill(LCDBlack);
 	
-	for (uint32_t i = 0; i < SCOPE_MAX_X; i++) {
-		float sine = sinf(2 * 3.1415 / SCOPE_MAX_X * i);
-		buff[i] = (sine + 1.) * 0.5 * SCOPE_MAX_Y;
-	}
+	init_adc();
+	
+	LED_SetOut(1);
+
 }
 
 void loop() {
-	draw_buffer(buff, 0xffff);
+	LPC_ADC->ADCR |= (0b001 << 24); // Start conversion now
+	
+	while(!(LPC_ADC->ADGDR >> 31)); // Wait for result
+	
+	uint16_t adc_val = (LPC_ADC->ADGDR >> 4) & 0xFFF;
+	
+	draw_buffer(buff, 0);
+	for (uint32_t i = 0; i < SCOPE_MAX_X; i++) {
+		buff[i] = adc_val * SCOPE_MAX_Y / 0xFFF;
+	}
+	draw_buffer(buff, 0b0000011111100000);
 }
 
 int main() {
