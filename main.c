07a099184a1882f1a254811582e34a926ddc4ea4
2urbanski <franek.urbanski@gmail.com>
2024-12-04 17:33:08 +0100

    ADC is now triggered by MAT0.1

--- main.c
+++ main.c
@@ -2,16 +2,26 @@
 #include "Board_LED.h"
 #include "LCDControl.h"
 
-#include <math.h>
-
 uint16_t buff[SCOPE_MAX_X] = {};
 
 void init_adc() {
-	LPC_SC->PCONP |= (1 << 12); // Enable ADC
-	LPC_ADC->ADCR = (1 << 1) 	| // ADC0.1
-									(1 << 21) | // ADC Operational
-									(5 << 8);   // Divide clock to 5 Mhz
+	LPC_SC->PCONP |= (1 << 12); 			// Enable ADC
+	LPC_ADC->ADCR = (1 << 1) 			| 	// ADC0.1
+									(1 << 21) 		| 	// ADC Operational
+									(5 << 8)  		| 	// Divide clock to 5 Mhz
+									(0b100 << 24);  	// Start conversion on MAT0.1 rising edge (default, bit 27)
+	
 	LPC_PINCON->PINSEL1 |= (1 << 16); // P0.24 as ADC0.1
+}
+
+void init_tim0() {
+	uint32_t us = 100; // 10 kHz sample rate
+	LPC_TIM0->TCR = 0;
+	LPC_TIM0->TCR = 1;
+	LPC_TIM0->PR = 0;
+	LPC_TIM0->MR0 = SystemCoreClock / (8000000 / us);
+	LPC_TIM0->MCR = 0b11;
+	LPC_TIM0->EMR |= (0b11 << 6); // When match occurs, toggle MAT0.1
 }
 	
 void setup() {
@@ -23,11 +33,12 @@ void setup() {
 	
 	LED_SetOut(1);
 
+	init_tim0();
+	
+	LED_SetOut(2);
 }
 
 void loop() {
-	LPC_ADC->ADCR |= (0b001 << 24); // Start conversion now
-	
 	while(!(LPC_ADC->ADGDR >> 31)); // Wait for result
 	
 	uint16_t adc_val = (LPC_ADC->ADGDR >> 4) & 0xFFF;
